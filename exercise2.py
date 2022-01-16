973d5c2ad06cb8668251db29017c177d298f36f6
Bogdanc0d3r <53659207+Bogdanc0d3r@users.noreply.github.com>
2022-01-16 19:53:47 +0200

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+def abs_difference(no1: int, no2:int ):
+    """This function returns the absolute value of the difference between no1 and
+    no2(these numbers could be both positive and negative because either way
+    the absolute value will return it positive."""
+    return abs(no1 - no2)
+
+help(abs_difference)
+print(abs_difference(50, -96))
