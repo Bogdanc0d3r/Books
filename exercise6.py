6f295ec7b72191a886720d495d5e2e964120d810
Bogdanc0d3r <53659207+Bogdanc0d3r@users.noreply.github.com>
2022-02-05 16:59:16 +0200

    Create exercise6.py

---
+++ exercise6.py
@@ -0,0 +1,10 @@
+def different(a, b):
+ """ (number, number) -> bool
+ Return True if a and b refer to the different values, or False,
+otherwise.
+ >>> different(0, 1)
+ True
+ >>> different(1, 1)
+ False
+ """
+ return a != b
