c2279622bb43bdcda8b01d099edfa2a2cfa5aeca
Bogdanc0d3r <53659207+Bogdanc0d3r@users.noreply.github.com>
2022-02-05 17:06:36 +0200

    Create exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,15 @@
+population = 135_000_000
+land_area = 78000
+if population < 100_000_000:
+  print(population)
+
+if population >= 100_000_000 and population <= 350_000_000:
+  print(population)
+  
+if (population / land_area) > 100:
+   print('Densely populated')
+  
+if (population / land_area) > 100:
+ print('Densely populated')
+else:
+ print('Sparsely populated')  
